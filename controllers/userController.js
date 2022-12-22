a24cc768853004bc3b317054bada22e5754c675c
Sabina Prajapati <sabina.prajapati777@gmail.com>
2022-12-22 13:03:08 +0545

    register users controller logics added

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,4 +26,43 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc- register the new user
+//@route -POST /api/users/create
+//@access -public
+const registerUser = asyncHandler(async (req, res) => {
+  //get the user inputs from req body
+  const { name, email, password, role } = req.body;
+  //find user with the same email
+  const existUser = await User.findOne({ email: email });
+
+  //if the user already exists , then throw the error
+  if (existUser) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  //else create the new user
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
+  //after the user is created, return json data
+  if (user) {
+    res.status(201).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
 export { authUser };
