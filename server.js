6740741a38d0ce4e1af98c823607b8fade6857ea
Sabina Prajapati <sabina.prajapati777@gmail.com>
2022-12-22 12:33:29 +0545

    database config added and database conenction made

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ import express from "express";
 
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import connectDB from "./config/db.js";
+import path from "path";
+import morgan from "morgan";
+import colors from "colors";
 
 // import { notfound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
-
+connectDB();
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
@@ -14,5 +18,9 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, console.log("Server running in port 4000"));
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log("Server running in port 5000"));
