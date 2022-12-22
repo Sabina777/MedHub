7a14128e24229f874c9df0087924aa196311a602
Sabina Prajapati <sabina.prajapati777@gmail.com>
2022-12-22 13:14:07 +0545

    routes added

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ import path from "path";
 import morgan from "morgan";
 import colors from "colors";
 
+import userRoutes from "./routes/userRoutes.js";
+
 // import { notfound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 connectDB();
@@ -21,6 +23,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+
+app.use("/users", userRoutes);
+
+// const __dirname = path.resolve();
+// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// app.use(notfound);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("Server running in port 5000"));
